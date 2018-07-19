755a2cfe0109b8a0fbc2598017c53561b310bd90
Feyex <36657009+feyex@users.noreply.github.com>
2018-07-19 21:46:49 +0100

    layout component removed

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,19 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NgbCarouselModule, NgbAlertModule } from '@ng-bootstrap/ng-bootstrap';
-import { LayoutComponent } from './layout/layout.component';
-
 
 @NgModule({
   imports: [
       CommonModule,
       NgbCarouselModule.forRoot(),
-      NgbAlertModule.forRoot(),
-      LayoutComponent
+      NgbAlertModule.forRoot()
+      
      
   ],
   declarations: [
-    LayoutComponent
+   
   ]
 })
 export class HomeModule {}
